6f87664d441e6f726d8276f01518b69c869e61b8
Gustavo Aguiar <gustavo_snap@hotmail.com>
2022-05-18 00:28:54 -0300

    Correção de bug visual

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -4,7 +4,6 @@ import * as Styles from './styles';
 import MaterialTable from '@material-table/core';
 import { GetData, UpdateData, DataProps } from '../../services/data';
 
-import TableIcons from './tableIcons';
 
 const Table = () => {
   const [columns, setColumns] = useState([
@@ -27,95 +26,90 @@ const Table = () => {
   ])
 
   const [data, setData] = useState<DataProps[] | []>([]);
-  const [icon, setIcon] = useState({});
+
+  const [loading, setLoading] = useState(false);
 
   // Função para obter dados da API
   const loadData = async () => {
     const response = await GetData();
     setData(response ?? []);
-  }
-
-  // Função para obter icones da tabela
-  const loadIcon = async () => {
-    const response = await TableIcons();;
-    setIcon(response ?? {});
+    setLoading(true);
   }
 
   // Effect para executar a consulta de dados na API assim que a página for carregada
   useEffect(() => {
     void loadData();
-    void loadIcon();
   }, []);
 
   return (
     <Styles.Container>
       <Styles.MaterialConfig>
-        <MaterialTable
-          title="Exemplo de consulta de dados da API"
-          icons={icon}
-          columns={columns}
-          data={data}
-          options={{
-            actionsColumnIndex: -1,
-            searchFieldStyle: {
-              backgroundColor: 'lightcyan',
-              padding: 5,
-            },
-            rowStyle: {
-              backgroundColor: 'lightcyan',
-            },
-            headerStyle: {
-              backgroundColor: 'darkcyan',
-              color: 'white',
-            },
-          }}
-          editable={{
-            // REMOVA ESTE COMENTÁRIO PARA HABILITAR INCLUSÃO DE DADOS NA TABELA
-            // onRowAdd: (newData) =>
-            //   new Promise((resolve, reject) => {
-            //     setTimeout(() => {
-            //       setData([...data, newData]);
+        {loading && (
+          <MaterialTable
+            title="Exemplo de consulta de dados da API"
+            columns={columns}
+            data={data}
+            options={{
+              actionsColumnIndex: -1,
+              searchFieldStyle: {
+                backgroundColor: 'lightcyan',
+                padding: 5,
+              },
+              rowStyle: {
+                backgroundColor: 'lightcyan',
+              },
+              headerStyle: {
+                backgroundColor: 'darkcyan',
+                color: 'white',
+              },
+            }}
+            editable={{
+              // REMOVA ESTE COMENTÁRIO PARA HABILITAR INCLUSÃO DE DADOS NA TABELA
+              // onRowAdd: (newData) =>
+              //   new Promise((resolve, reject) => {
+              //     setTimeout(() => {
+              //       setData([...data, newData]);
 
-            //       resolve();
-            //     }, 1000);
-            //   }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  const dataUpdate = [...data];
-                  const index = oldData.id;
-                  dataUpdate[index - 1] = newData;
-                  setData([...dataUpdate]);
+              //       resolve();
+              //     }, 1000);
+              //   }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve) => {
+                  setTimeout(() => {
+                    const dataUpdate = [...data];
+                    const index = oldData.id;
+                    dataUpdate[index - 1] = newData;
+                    setData([...dataUpdate]);
 
-                  // Função async para enviar score atualizado na API via put
-                  const submit = async (data: DataProps) => {
-                    try {
-                      await UpdateData(data);
-                    } catch (error: any) {
-                      console.log('Deu um erro:', error);
+                    // Função async para enviar score atualizado na API via put
+                    const submit = async (data: DataProps) => {
+                      try {
+                        await UpdateData(data);
+                      } catch (error: any) {
+                        console.log('Deu um erro:', error);
+                      }
                     }
-                  }
 
-                  // Executar função async
-                  submit(newData);
+                    // Executar função async
+                    submit(newData);
 
-                  resolve(newData);
-                }, 1000);
-              }),
-            // REMOVA ESTE COMENTÁRIO PARA HABILITAR EXCLUSÃO DE DADOS NA TABELA
-            // onRowDelete: (oldData) =>
-            //   new Promise((resolve, reject) => {
-            //     setTimeout(() => {
-            //       const dataDelete = [...data];
-            //       const index = oldData.tableData.id;
-            //       dataDelete.splice(index, 1);
-            //       setData([...dataDelete]);
+                    resolve(newData);
+                  }, 1000);
+                }),
+              // REMOVA ESTE COMENTÁRIO PARA HABILITAR EXCLUSÃO DE DADOS NA TABELA
+              // onRowDelete: (oldData) =>
+              //   new Promise((resolve, reject) => {
+              //     setTimeout(() => {
+              //       const dataDelete = [...data];
+              //       const index = oldData.tableData.id;
+              //       dataDelete.splice(index, 1);
+              //       setData([...dataDelete]);
 
-            //       resolve();
-            //     }, 1000);
-            //   })
-          }}
-        />
+              //       resolve();
+              //     }, 1000);
+              //   })
+            }}
+          />)}
       </Styles.MaterialConfig>
     </Styles.Container>
   )
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,15 +4,15 @@ const api = axios.create({
   baseURL: 'https://6283fcc33060bbd347330cbf.mockapi.io/api',
 });
 
-api.interceptors.request.use(async (config: any) => {
-  const token = '';
+// api.interceptors.request.use(async (config: any) => {
+//   const token = '';
 
-  if (token) {
-    config.headers.common = { Authorization: `Bearer ${token}` };
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+//   if (token) {
+//     config.headers.common = { Authorization: `Bearer ${token}` };
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
 
-  return config;
-})
+//   return config;
+// })
 
 export default api;
